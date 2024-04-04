21a00f0d2037c9e1b547fc3e9da37e9b8ad1a847
Bijan <bijan2059@gmail.com>
2024-04-04 09:17:01 +0545

    added comment to banner and header of category

--- src/app/components/BannerComponents/Banner.jsx
+++ src/app/components/BannerComponents/Banner.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
 const Banner = () => {
-    
+
   return (
     <div className="bannerContainer w-full px-3 mt-4">
-    <div className="banner w-full h-20 bg-gray-200 rounded md:w-3/4 md:h-60 md:mx-auto">
+      <div className="banner w-full h-20 bg-gray-200 rounded md:w-3/4 md:h-60 md:mx-auto">
         <img src='/shoeBanner.jpg' className="w-full h-full object-cover rounded" ></img>
-    </div>
+      </div>
     </div>
   )
 }
--- src/app/components/categoryComponents/CategoryHeader.jsx
+++ src/app/components/categoryComponents/CategoryHeader.jsx
@@ -6,6 +6,7 @@ const CategoryHeader = () => {
     const [showModal, setShowModal] = useState(false);
     return (
         <>
+        {/* contains the sorting and filtering btns  */}
         <div className="flex items-center justify-between mt-2 px-3 h-14 md:w-3/4 md:mx-auto md:justify-end">
             <div className="flex w-5/12 bg-gray-200 hover:bg-gray-300 transition-colors duration-300 ease-in  px-2 py-1 justify-between items-center cursor-pointer rounded shadow-[0_3px_10px_rgb(0,0,0,0.2)] md:w-auto md:mr-4">
                 <span className='md:mr-2'>Sort By </span>
@@ -16,6 +17,7 @@ const CategoryHeader = () => {
                 <FaFilter />
             </div>
         </div>
+        {/* contains the popup modal  */}
         {showModal && <CategoryPopup setShowModal={setShowModal} />}
         </>
         
